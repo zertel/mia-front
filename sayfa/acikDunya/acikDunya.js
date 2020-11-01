91c286387ff74f52c4ac551f40a1bf6575471764
zertel <zertel@orhantutum.com.tr>
2020-11-01 20:12:20 +0300

    sayfa yuklendikten sonra js ve css dosyalarini yukle

---
+++ sayfa/acikDunya/acikDunya.js
@@ -0,0 +1,5 @@
+mia.acikDunyaOnLoad = function(){
+	cl("açık dünya fonksiyonu çalıştı");
+}
+
+
---
+++ sayfa/canavarlarim/canavarlarim.js
@@ -0,0 +1,5 @@
+mia.canavarlarimOnLoad = function(){
+	for(i=1; i<=20; i++){
+		parcaYukle('canavar-kart','#canavarlarim',i);
+	}
+}
