0ce5c2e4b2805b023526e2d3e1481d76fa9069bb
orhantutum@netadim.com.tr <zertel@orhantutum.com.tr>
2021-10-23 22:15:15 +0300

    app.js uyelik girisi yonlendirmesi

--- js/app.js
+++ js/app.js
@@ -4,10 +4,30 @@ cl("app.js yüklendi");
 
 
 window.onload = function(){
-	mia.sayfaYukle('anasayfa');
+
+
+	// Sayfa ilk yüklendiğinde, üyelik girişinin olup olmadığını, ajax isteği ile öğren
+	ajaxGet('http://localhost:83/hesap/giris',function(donenCevap){
+
+		if(donenCevap){
+			cl("Hesap giriş bilgileri geldi");
+
+			// text yığını olarak dönen json verisini parçala ve objeye dönüştür 
+			var donenCevapJson = JSON.parse(donenCevap);
+
+			if(donenCevapJson.sonuc == 1){
+				mia.sayfaYukle('acikDunya');
+			}
+			else{
+				mia.sayfaYukle('anasayfa');
+			}
+		}
+		
+	});
+
+
 	//mia.sayfaYukle('uyelikGirisi');
 	//mia.sayfaYukle('hesapOlustur');
-	//mia.sayfaYukle('acikDunya');
 	//mia.sayfaYukle('canavarlarim');
 	//mia.sayfaYukle('intro');
 };
