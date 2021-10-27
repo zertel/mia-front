e782e70677e7849a390980e0a8b7ed0fc41887b0
orhantutum@netadim.com.tr <zertel@orhantutum.com.tr>
2021-10-27 21:49:10 +0300

    canavarlarim sayfasi css guncellendi

--- sayfa/canavarlarim/canavarlarim.js
+++ sayfa/canavarlarim/canavarlarim.js
@@ -3,12 +3,13 @@ mia.canavarlarim={};
 // canavarlarim page onload function
 mia.canavarlarim.yuklendiginde = function(){
 
-	// api üzerinden canavarlarım verilerini getir
-	ajaxGet('http://localhost/mia-api/canavarlarim/',function(donenCevap){
 
+	// api üzerinden canavarlarım verilerini getir
+	ajaxGet('http://localhost:83/hesap/canavarlarim',function(donenCevap){
 		if(donenCevap){
 			cl("canavarlar api üzerinden yüklendi (URL http://localhost/mia-api/monsters/)");
 
+			/*/
 			// text yığını olarak dönen json verisini parçala ve objeye dönüştür 
 			var canavarlarim = JSON.parse(donenCevap);
 
@@ -16,7 +17,8 @@ mia.canavarlarim.yuklendiginde = function(){
 			for(i=0; i<canavarlarim.length; i++){
 				mia.parcaYukle('canavar-kart','#canavarlarim', canavarlarim[i] );
 			}
+			/*/
 		}
-		
+
 	});
 }
